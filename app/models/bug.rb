982f894044c17d75cb9b4e29300bb9bb1f3d46ea
Ziayan Javed <ziayan.javed@devsinc.com>
2022-05-06 23:25:47 +0500

    Custom validator to make a bug title unique throughout its project

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,4 +1,17 @@
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    valid = true
+
+    record.project.bugs.each do |bug|
+      valid = false if bug.title.downcase == value.downcase
+    end
+    record.errors[attribute] << "Project already contains a bug with this #{attribute}" unless valid
+  end
+end
+
 class Bug < ApplicationRecord
+  validates :title, title: true
+
   belongs_to :creator, class_name: "Qa", foreign_key: 'qa_id'
   belongs_to :developer, optional: true
   belongs_to :project
