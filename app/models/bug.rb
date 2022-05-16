10bc4a729cea34e2e9a02dd49eee862143c5b960
Ziayan Javed <ziayan.javed@devsinc.com>
2022-05-16 21:28:10 +0500

    Move TitleValidator into separate validators directory

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,14 +1,3 @@
-class TitleValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    valid = true
-
-    record.project.bugs.each do |bug|
-      valid = false if bug.title.downcase == value.downcase
-    end
-    record.errors[attribute] << "Project already contains a bug with this #{attribute}" unless valid
-  end
-end
-
 class Bug < ApplicationRecord
   enum category: { feature: 0, bug: 1 }
   enum status: { created: 0, started: 1, completed: 2, resolved: 3 }
